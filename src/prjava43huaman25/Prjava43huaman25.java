2beec800a0104e3266fbc779038a07ec6881b4a0
daw2-huaman25 <kevinhuamanpinto@gmail.com>
2024-10-22 17:04:22 +0200

    Versió 0.7-C del projecte prjava43huaman25. Branca main (omaster) m modificada

--- src/prjava43huaman25/Prjava43huaman25.java
+++ src/prjava43huaman25/Prjava43huaman25.java
@@ -2,6 +2,7 @@ package prjava43huaman25;
 import java.awt.*; // using AWT containers and components
 import java.awt.event.*; // using AWT events and listener interfaces
 // An AWT GUI program inherits the top-level container java.awt.Frame
+import java.sql.*;
 import java.io.*;
 public class Prjava43huaman25 extends Frame
 implements ActionListener, WindowListener {
